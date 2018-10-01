2778ab20a4f5192cea46a807f06dc33ddc3d0e06
Alan Vieceli <alansvieceli@gmail.com>
2018-10-01 12:54:57 -0300

    outras meneiras de executar comparable

--- teste_comparable/src/aplicacao/Programa.java
+++ teste_comparable/src/aplicacao/Programa.java
@@ -1,6 +1,7 @@
 package aplicacao;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entidades.Produto;
@@ -8,6 +9,8 @@ import entidades.Produto;
 public class Programa {
 
 	public static void main(String[] args0) {
+		
+		//• Comparator objeto de classe separada
 		
 		List<Produto> lista = new ArrayList<>();
 		
@@ -22,5 +25,57 @@ public class Programa {
 			System.out.println(produto);
 		}
 		
+		System.out.println("--------------------");
+		
+		
+		//• Comparator objeto de classe anônima
+		List<Produto> lista2 = new ArrayList<>();
+		
+		lista2.add(new Produto("TV", 900.00));
+		lista2.add(new Produto("Notebok", 1200.00));
+		lista2.add(new Produto("Tablet", 450.00));
+		lista2.add(new Produto("Celular", 1950.00));
+		
+		Comparator<Produto> comp = new Comparator<Produto>() {
+
+			@Override
+			public int compare(Produto p1, Produto p2) {				// 
+				return p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());
+			}		
+		};
+		
+		lista2.sort(comp);
+		
+		for (Produto produto : lista2) {
+			System.out.println(produto);
+		}
+		
+		System.out.println("--------------------");
+		
+		//usando expressao lambda
+		List<Produto> lista3 = new ArrayList<>();
+		
+		lista3.add(new Produto("TV", 900.00));
+		lista3.add(new Produto("Notebok", 1200.00));
+		lista3.add(new Produto("Tablet", 450.00));
+		lista3.add(new Produto("Celular", 1950.00));
+		
+		//• Comparator objeto de expressão lambda com chaves
+		//Comparator<Produto> comp2 = (p1, p2) -> {
+		//	return p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());			
+		//};
+		
+		//• Comparator objeto de expressão lambda sem chaves		
+		//Comparator<Produto> comp2 = (p1, p2) -> p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());
+		
+		//lista3.sort(comp2);
+		
+		//• Comparator expressão lambda "direto no argumento"
+		
+		lista3.sort((p1, p2) -> p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase()));
+		
+		for (Produto produto : lista3) {
+			System.out.println(produto);
+		}
 	}
 }
