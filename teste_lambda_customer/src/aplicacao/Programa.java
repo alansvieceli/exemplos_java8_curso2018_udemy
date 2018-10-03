ca38ce05ddb4d1deec29536ec568ba803886fe8e
Alan Vieceli <alansvieceli@gmail.com>
2018-10-03 13:26:21 -0300

    ajustes nos nomes e add lambda customer

---
+++ teste_lambda_customer/src/aplicacao/Programa.java
@@ -0,0 +1,50 @@
+package aplicacao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import entidades.Produto;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Produto> lista = new ArrayList<>();
+		lista.add(new Produto("Tv", 900.00));
+		lista.add(new Produto("Mouse", 50.00));
+		lista.add(new Produto("Tablet", 350.50));
+		lista.add(new Produto("HD Case", 80.90));
+		
+		for (Produto produto : lista) {
+			System.out.println(produto);
+		}
+		
+		System.out.println("---------------------");
+		
+		//Implementação da interface
+		//lista.forEach(new AtualizarPreco());
+		
+		//Reference method com método estático
+		//lista.forEach(Produto::staticAtualizarPreco);
+		
+		//Reference method com método não estático
+		//lista.forEach(Produto::notstaticAtualizarPreco);
+		
+		//Expressão lambda declarada
+		/*
+		Consumer<Produto> cons = p ->{
+			p.setPreco(p.getPreco() * 1.1);
+		};
+		lista.forEach(cons);
+		*/
+		
+		//Expressão lambda inline
+		lista.forEach(p -> p.setPreco(p.getPreco() * 1.1));
+		
+		lista.forEach(System.out::println);
+
+	}
+
+}
---
+++ teste_lambda_customer/src/utils/AtualizarPreco.java
@@ -0,0 +1,14 @@
+package utils;
+
+import java.util.function.Consumer;
+
+import entidades.Produto;
+
+public class AtualizarPreco implements Consumer<Produto> {
+
+	@Override
+	public void accept(Produto p) {
+		p.setPreco(p.getPreco() * 1.1);				
+	}
+
+}
